1076b50d312b6654b7a9ce3efed6546c08db6743
ykbboom <1747603726@qq.com>
2018-05-22 23:01:53 +0800

    初始化flask-sqlalchemy

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -3,11 +3,15 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/freesiawebsite'
+
     Bootstrap(app)
+    db = SQLAlchemy(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
