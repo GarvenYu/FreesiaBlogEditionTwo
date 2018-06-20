b6a22320e579bf8830cc55db7f7079e430060e29
ykbboom <1747603726@qq.com>
2018-06-20 19:11:26 +0800

    服务端将数据回传给select供选择(博客类别)

--- venv/app/main/views.py
+++ venv/app/main/views.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import render_template, session, redirect, url_for, request
+from flask import render_template, session, redirect, url_for, request, jsonify
+import json
+import logging
 from . import main
-from .forms import BlogForm
+from ..models import Category
+
+
+logger = logging.getLogger()
 
 
 @main.route('/mainPage', methods=['GET'])
@@ -14,13 +19,15 @@ def index():
 
 @main.route('/write', methods=['GET', 'POST'])
 def write_blog():
-    return render_template('blog/write_blog.html')
+    categories = Category.query.all()
+    option_list = [dict(id=category.id, name=category.name) for category in categories]
+    return render_template('blog/write_blog.html', option_list=option_list)
 
 
 @main.route('/saveBlog', methods=['POST'])
 def save_blog():
-    title = request.form['blog_title']
-    summary = request.form['blog_summary']
-    tag = request.form['states[]']
-    content = request.form['content']
-    return render_template('home/mainPage.html', title=title, summary=summary, tag=tag, content=content)
+    data = request.get_data()
+    dict_data = json.loads(data)
+    # {'blog_title': '得到', 'blog_summary': '对对对', 'states': ['AL', 'WY'], 'content': ''}
+
+    return render_template('home/mainPage.html')
--- venv/app/models.py
+++ venv/app/models.py
@@ -7,22 +7,25 @@ from datetime import datetime
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
     summary = db.Column(db.String(20))
     content = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now())
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))  # 博客分类
     category = db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
 
-    def __init__(self, content, tag):
+    def __init__(self, title, summary, content, category):
+        self.title = title
+        self.summary = summary
         self.content = content
-        self.tag = tag
+        self.category = category
 
     def __repr__(self):
         return '<Blog %r>' % self.tag
 
 
 class Category(db.Model):
-    id = db.Column(db.INTEGER, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
 
     def __init__(self, name):
