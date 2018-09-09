62d7910eca7221be10309231718c90cec797cf56
GarvenYu <1747603726@qq.com>
2018-09-09 22:36:36 +0800

    use gevent as WSGI server

--- venv/app/manager.py
+++ venv/app/manager.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from app import create_app
+from gevent.pywsgi import WSGIServer
 
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    server = WSGIServer(('127.0.0.1', 8000), app)
+    server.serve_forever()
